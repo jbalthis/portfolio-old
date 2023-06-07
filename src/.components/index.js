7454bcde7297a7ee114132936deb86eb6ae7a705
jbalthis <balthis.jason@gmail.com>
2023-06-07 06:55:23 -0400

    test deploy

--- src/.components/index.js
+++ src/.components/index.js
@@ -5,7 +5,7 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
+import Feedbacks from './Testimonials';
 import Contact from './Contact';
 
 export {
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import{
+  About,
+  Contact,
+  Experience,
+  Testimonials,
+  Hero,
+  Navbar,
+  Tech,
+  Projects,
+  StarsCanvas
+} from './components';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    /* SPA routing context provider begins */
+    <BrowserRouter>
+        {/* page container */}
+      <div className="relative z-0 bg-primary">        
+        {/*  page header  */}
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>                              {/*         routing table          */}
+        {/*  page body  */}                 {/**********************************/}
+        <About />                           {/* <SPA_BASE_URL> + /about        */}
+        <Experience />                      {/* <SPA_BASE_URL> + /experience   */}
+        <Tech />                            {/* <SPA_BASE_URL> + /tech         */}
+        <Projects />                        {/* <SPA_BASE_URL> + /projects     */}
+        <Testimonials />                    {/* <SPA_BASE_URL> + /testimonials */}
+                                            {/*                                */}
+        {/* ext. page body */}              {/*                                */}
+        <div className="relative z-0">      {/*                                */}
+        {/* contact form */}                {/*                                */}
+          <Contact />                       {/* <SPA_BASE_URL> + /contact      */}
+          <StarsCanvas />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
+    /* SPA routing context provider ends */
   )
 }
-
 export default App
