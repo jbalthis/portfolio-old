31fabdffd6be9407deed48f3d89e1ac2d928028d
jbalthis <balthis.jason@gmail.com>
2023-06-09 02:39:43 +0000

    build: hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop cutting-edge applications with 
-            clean and modern user interfaces and 3D visuals
+            clean and responsive user interfaces and 3D visuals
           </p>
         </div>
       </div>
